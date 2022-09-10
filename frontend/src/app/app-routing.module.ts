448a38d86918fc446521cbd0d6103cee4105c46c
Chirayu Chomsri <chirayu.chomsri@gmail.com>
2022-09-11 00:31:35 +0700

    add search component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+import { SigninComponent } from './components/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: 'signin', component: SigninComponent },
+  { path: 'search', component: SearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
